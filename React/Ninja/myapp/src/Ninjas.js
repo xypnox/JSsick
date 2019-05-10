6074da6a967edd6932419398df1a2a94dc602bd7
xypnox <xypnox@gmail.com>
2019-05-10 14:08:36 +0530

    Created functional UI Components and destrut props

--- React/Ninja/myapp/src/Ninjas.js
+++ React/Ninja/myapp/src/Ninjas.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninja extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function Ninja({ ninjas }) {
+  const ninjaList = ninjas.map(ninja => {
+    return (
+      <div className="Ninja" key={ninja.id}>
+        <h1>Name: {ninja.name}</h1>
+        <p>Age: {ninja.age} </p>
+        <p>Belt: {ninja.belt}</p>
+      </div>
+    );
+  });
 
-  render() {
-    console.log(this.props);
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map(ninja => {
-      return (
-        <div className="Ninja" key={ninja.id}>
-          <h1>Name: {ninja.name}</h1>
-          <p>Age: {ninja.age} </p>
-          <p>Belt: {ninja.belt}</p>
-        </div>
-      );
-    });
-
-    return <div className="ninjas">{ninjaList}</div>;
-  }
+  return <div className="ninjas">{ninjaList}</div>;
 }
 
 export default Ninja;
