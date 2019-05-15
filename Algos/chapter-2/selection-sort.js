54056cd7f10097234ff46564ff67058beb11ea18
xypnox <xypnox@gmail.com>
2019-05-15 23:30:53 +0530

    Added a few comments for selection sort

--- Algos/chapter-2/selection-sort.js
+++ Algos/chapter-2/selection-sort.js
@@ -5,12 +5,16 @@ for (let i = 0; i < A.length - 1; i++) {
   let pos = i;
 
   for (let j = i; j < A.length; j++) {
+    // in this loop we store the smallest of subarray A[i..n]
+    // in element and position in pos
     if (element > A[j]) {
       element = A[j];
       pos = j;
     }
   }
-
+  // here we swap A[pos] and A[i] we are helped by the fact
+  // that the value in A[pos] is already stored in element
+  // Hence we can overwrite that value
   A[pos] = A[i];
   A[i] = element;
 }
