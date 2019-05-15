7e50b60d641d8f754b99e9f25b1458696d9c947a
xypnox <xypnox@gmail.com>
2019-05-15 22:38:24 +0530

    Added insertion sort

---
+++ Algos/insertion-sort.js
@@ -0,0 +1,18 @@
+let i;
+let A = [5, 8, 6, 3, 7, 1, 2, 4]
+
+console.log(A);
+
+for (let j = 1; j < A.length; j++) {
+  const key = A[j];
+  i = j - 1;
+
+  while (i >= 0 && A[i] > key) {
+    A[i + 1] = A[i];
+    i = i - 1;
+  }
+
+  A[i + 1] = key;
+}
+
+console.log(A)
